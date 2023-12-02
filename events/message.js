0aa3d03ddadafb9c7b209501db127fa817724a84
Carter Hill <carter@adaptiveagriculture.ca>
2023-12-02 17:05:43 -0600

    bot now deletes messages that contains words in the filter.json

--- events/message.js
+++ events/message.js
@@ -1,9 +1,23 @@
 const { Events } = require('discord.js');
+const filter = require('../filter.json');
 
 module.exports = {
 	name: Events.MessageCreate,
-	once: true,
+	once: false,
 	execute(message) {
-		console.log(`Message: ${message}`);
+		let low = message.toString().toLowerCase()
+		let words = low.replace(/[.,\/?#!$%\^&\*;:{}=\-_`~()]/g,"").split(" ")
+		filter.forEach(async word => {
+			if (words.includes(word)) {
+				try {
+					const fetchedMessage = await message.channel.messages.fetch(message.id);
+					await fetchedMessage.delete();
+					console.log(`Deleted message with ID ${message.id}`);
+				} catch (error) {
+					console.error(`Failed to delete message with ID ${message.id}: ${error}`);
+				}
+				return
+			}
+		})
 	},
 };
